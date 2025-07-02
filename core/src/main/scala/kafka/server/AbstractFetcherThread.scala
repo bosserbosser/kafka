7b80373cdf2795c719189a166441a1c80b3c603d
姚亚楠 <yanan@datatell.com.cn>
2025-07-02 11:47:35 +0800

    more log after ech fetch turn

--- core/src/main/scala/kafka/server/AbstractFetcherThread.scala
+++ core/src/main/scala/kafka/server/AbstractFetcherThread.scala
@@ -134,7 +134,14 @@ abstract class AbstractFetcherThread(name: String,
     val time3 = System.currentTimeMillis()
     val sleepTime = 1000 * 1
     Thread.sleep(sleepTime)
-    info(s"maybeFetch over, timeCost: ${time3-time1}, timeCost_build: ${time2-time1}, timeCost_fetch: ${time3-time2}, sleepTime: ${sleepTime}")
+    val (partitions, topics) = fetchRequestOpt.map(
+      fetchRequest => {
+        val partitionSet: util.Set[TopicPartition] = fetchRequest.partitionData.keySet()
+        val topics: Long = partitionSet.stream().map(_.topic()).distinct().count()
+        (partitionSet.size(), topics.toInt)
+      }
+    ).getOrElse((0,0))
+    info(s"maybeFetch over, partitions: ${partitions}, topics: ${topics}, x: ${} timeCost: ${time3-time1}, timeCost_build: ${time2-time1}, timeCost_fetch: ${time3-time2}, sleepTime: ${sleepTime}")
   }
 
   // deal with partitions with errors, potentially due to leadership changes
