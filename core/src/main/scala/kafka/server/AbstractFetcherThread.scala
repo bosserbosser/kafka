fea95183a8264df4db387c374645cbf828ad2dd8
姚亚楠 <yanan@datatell.com.cn>
2025-07-07 11:55:48 +0800

    more log

--- core/src/main/scala/kafka/server/AbstractFetcherThread.scala
+++ core/src/main/scala/kafka/server/AbstractFetcherThread.scala
@@ -150,8 +150,8 @@ abstract class AbstractFetcherThread(name: String,
     ).getOrElse(Map.empty)
     val fetchSizeTotal = topicSizeMap.values.sum
     val sleepTime = 1000 * 10
-    info(s"maybeFetch over, name: ${name}, partitions: ${partitions}, topics: ${topics}, timeCost: ${time3-time1}, timeCost_build: ${time2-time1}, timeCost_fetch: ${time3-time2}, sleepTime: ${sleepTime}, " +
-        s"fetchSizeTotal: ${fetchSizeTotal*1.0/1024/1024}%.2fMB, topicSizeMap: ${topicSizeMap.toSeq.sortWith(_._2 > _._2).map(pair=>(pair._1,s"${pair._2*1.0/1024/1024}%.2fMB"))}")
+    info(f"maybeFetch over, name: ${name}, partitions: ${partitions}, topics: ${topics}, timeCost: ${time3-time1}, timeCost_build: ${time2-time1}, timeCost_fetch: ${time3-time2}, sleepTime: ${sleepTime}, " +
+        f"fetchSizeTotal: ${fetchSizeTotal*1.0/1024/1024}%.2fMB, topicSizeMap: ${topicSizeMap.toSeq.sortWith(_._2 > _._2).map(pair=>(pair._1,s"${pair._2*1.0/1024/1024}%.2fMB"))}")
     Try { Thread.sleep(sleepTime) }.foreach(_ =>{})
   }
 
